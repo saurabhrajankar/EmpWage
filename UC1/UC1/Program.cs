902390a6ab5b9c4beeccf1f702e04f15b672014f
saurabhrajankar <saurabhrajankar777@gmail.com>
2022-10-02 22:43:32 +0530

    UC1UC1

---
+++ UC1/UC1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UC1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation Program");
+            //Constant
+            int IS_FULL_TIME = 1;
+            Random random = new Random();//pseudo-random number generator
+
+            //computation
+            int empcheck = random.Next(0, 2);
+            if (empcheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent!");
+            }
+            Console.ReadLine();
+        }
+    }
+}
